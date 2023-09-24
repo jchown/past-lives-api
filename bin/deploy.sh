03032541bfa926e5450ad543cc6c7bfacc9e060b
Jason Chown <j.chown@vtime.net>
2023-09-24 14:23:38 +0100

    Double-checked it works locally

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-bin/build.sh
+set -e
+
+./bin/build.sh
+
+echo "Logging in to AWS"
 
 repo="368159677168.dkr.ecr.eu-west-2.amazonaws.com/past-lives-api"
 hash=$(git rev-parse --short HEAD)
@@ -8,8 +12,12 @@ hash=$(git rev-parse --short HEAD)
 aws="docker run --rm -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY amazon/aws-cli"
 $aws ecr get-login-password --region eu-west-2 | docker login --username AWS --password-stdin 368159677168.dkr.ecr.eu-west-2.amazonaws.com
 
+echo "Tagging past-lives-api"
+
 docker tag past-lives-api "$repo:$hash"
 docker tag past-lives-api "$repo:latest"
+
+echo "Pushing past-lives-api"
 
 docker push "$repo:$hash"
 docker push "$repo:latest"
