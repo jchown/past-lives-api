a9bf9b03c61e158d281d113d7f109feec2b374c1
Jason Chown <j.chown@vtime.net>
2024-01-02 22:29:53 +0000

    Needs to be passed through to the container too

--- bin/build.sh
+++ bin/build.sh
@@ -8,6 +8,7 @@ docker run --rm \
   --name past-lives-api \
   -v "$(pwd)":/src \
   -w /src \
+  -e SENTRY_AUTH_TOKEN \
   maven:3-eclipse-temurin-17 \
   mvn compile dependency:copy-dependencies -DincludeScope=runtime
 
